bb7c513dc88ad024c4023a1a851f88ce9a7d910f
Yito Mok <yitomok@PeterMB.local>
2014-07-04 12:12:36 +0800

    Update to answer 11-13

--- learnyounode/myprogram11.js
+++ learnyounode/myprogram11.js
@@ -1,10 +1,7 @@
 var http = require('http')
-var map = require('through2-map')
+var fs = require('fs')
 var server = http.createServer(function (req, res) {
-  if (req.method != 'POST')
-    return res.end('send me a POST\n')
-  req.pipe(map(function (chunk) {
-    return chunk.toString().toUpperCase()
-  })).pipe(res)
+  res.writeHead(200, {'content-type' : 'text/plain'})
+  fs.createReadStream(process.argv[3]).pipe(res)
 })
 server.listen(Number(process.argv[2]))
--- learnyounode/myprogram12.js
+++ learnyounode/myprogram12.js
@@ -1,21 +1,10 @@
-var url = require('url')
 var http = require('http')
 var map = require('through2-map')
 var server = http.createServer(function (req, res) {
-  if (req.method != 'GET')
-    return res.end('send me a GET\n')
-  var o = url.parse(req.url, true)
-  if (o.pathname === '/api/parsetime') {
-    var d = new Date(o.query.iso)
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ 'hour': d.getHours(), 'minute': d.getMinutes(), 'second': d.getSeconds() }))
-  } else if (o.pathname === '/api/unixtime') {
-    var d = new Date(o.query.iso)
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ 'unixtime': d.getTime() }))
-  } else {
-    res.writeHead(404)
-    res.end()
-  }
+  if (req.method != 'POST')
+    return res.end('send me a POST\n')
+  req.pipe(map(function (chunk) {
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
 })
 server.listen(Number(process.argv[2]))
